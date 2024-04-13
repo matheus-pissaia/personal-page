2ac3c7f967ba28d5697fcad2ac69da0d3bf6ec75
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-04-13 17:27:18 -0300

    feat: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors: {
+        white: '#f9f9f9',
+        black: '#131313',
+        transparent: 'transparent',
+        gray: {
+            300: '#bfbfbf',
+            600: '#484848',
+        },
+        blue: '#07007f',
+    },
+
+    fontSize: {
+        12: '12px',
+        14: '14px',
+        16: '16px',
+    },
+
+    extend: {
+        borderColor: {
+            duotone: '#f9f9f9 #484848 #484848 #f9f9f9',
+        },
+    },
+  },
+  plugins: [],
+}
+
