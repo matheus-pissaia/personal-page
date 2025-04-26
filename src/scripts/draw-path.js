8a6ecc7311777fc931439c1ac73138378d7f9eb0
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-04-26 17:24:10 -0300

    feat: update UI (WIP)

---
+++ src/scripts/draw-path.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param {HTMLElement} fromElement
+ * @param {HTMLElement} toElement
+ */
+function drawPath(fromElement, toElement) {
+    const svgEl = document.getElementById('svgEl')
+
+    if (!svgEl)
+        return
+
+    const pathEl = svgEl.children[0]
+    pathEl.classList.remove('animate-path')
+
+    const fromElementX = fromElement.offsetLeft + fromElement.offsetWidth
+    const fromElementY = fromElement.offsetTop + (fromElement.offsetHeight / 2)
+
+    const toElementX = toElement.offsetLeft
+    const toElementY = toElement.offsetTop + (toElement.offsetHeight / 2)
+    const middleX = (fromElementX + toElementX) / 2
+
+    pathEl.classList.add('animate-path')
+    pathEl.setAttribute(
+        'd',
+        `M${fromElementX} ${fromElementY} L${middleX} ${fromElementY} L${middleX} ${toElementY}`
+    )
+}
