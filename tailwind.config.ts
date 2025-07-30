d0e79045ec6d85c0fce4c1de73b1a0535a3d0dc4
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-07-30 00:27:38 -0300

    feat: add font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,6 +29,10 @@ export default {
             16: '16px',
             18: '18px',
             20: '20px',
+            24: '24px',
+            28: '28px',
+            32: '32px',
+            36: '36px',
         },
 
         fontWeight: {
