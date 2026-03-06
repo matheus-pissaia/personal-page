cc24c179fdf5fa655274a88e68c83edded244e23
Matheus Pissaia <pissaia.matheus03@gmail.com>
2026-03-05 21:50:33 -0300

    style: set fixed width to avoid content shifting

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -4,7 +4,7 @@
         bg-green-500 text-28 text-green-900 leading-none"
     >
         <span>PERSONAL TERMINAL</span>
-        <time id="time">{{ currentTime }}</time>
+        <time id="time" class="w-16">{{ currentTime }}</time>
     </div>
 </template>
 
