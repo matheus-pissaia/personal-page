384d765b0649743d7e1e1349077a2ccdc67c5bb6
Matheus Pissaia <pissaia.matheus03@gmail.com>
2026-03-09 23:40:45 -0300

    feat: add github link in top bar

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,15 +1,29 @@
 <template>
     <div
-        class="flex items-center justify-between px-4
-        bg-green-500 text-28 text-green-900 leading-none"
+        class="flex items-center justify-between px-4 pr-1
+        bg-green-500 text-28 text-green-900 leading-none bloom-effect"
     >
         <span>PERSONAL TERMINAL</span>
-        <time id="time" class="w-16">{{ currentTime }}</time>
+
+        <a
+            href="https://github.com/matheus-pissaia"
+            title="GitHub"
+            target="_blank"
+            rel="noopener"
+        >
+            <GitHub class="size-6 text-green-900/90" />
+        </a>
+
+        <div class="flex items-center gap-1">
+            <time id="time" class="w-[8ch]">{{ currentTime }}</time>
+            <div class="w-4 self-stretch my-1 bg-green-900/70" />
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
     import { useTime } from '@/composables/time'
+    import GitHub from './GitHub.vue'
 
     const { currentTime } = useTime()
 </script>
