4d0c73a600ed05553609c5c297f440da20657861
Matheus Pissaia <pissaia.matheus03@gmail.com>
2026-03-18 00:26:40 -0300

    feat: add `ABOUT THIS WEBSITE` section

--- src/components/Tabs/About/AboutThisWebsite.vue
+++ src/components/Tabs/About/AboutThisWebsite.vue
@@ -1,3 +1,63 @@
 <template>
-    <p>TODO</p>
+    <div class="space-y-6">
+        <p>
+            So, you got interested in this website, huh? Well, let me tell you a
+            bit about it...
+        </p>
+
+        <p>
+            Everything started when I wanted to make my personal website, but
+            I didn't had any kind of design in mind. I was looking for something
+            simple, yet cool and nostalgic like an old computer terminal.
+        </p>
+
+        <p>
+            So I remembered this one game I used to play called "Alien Isolation",
+            it had an amazing ambiance, soundtrack and design straight out from
+            the movies, no doubt a masterpiece of a game. It truly felt like I
+            was trying to survive from the Xenomorph.
+        </p>
+
+        <img
+            src="/images/alien.gif"
+            alt=""
+            class="mx-auto [mask:linear-gradient(to_right,transparent,white_15%,white_85%,transparent)]"
+        >
+
+        <p>
+            Non-theless, the game used this interactable terminals as a way to
+            provide lore via logs and e-mails, as well as controlling some other
+            stuff. So I decided to take some inspiration from it and create the
+            theme of this website around it. Check out below the original
+            terminal interface from the game:
+        </p>
+
+        <img
+            src="/images/terminal.gif"
+            alt=""
+            class="mx-auto [mask:linear-gradient(to_right,transparent,white_15%,white_85%,transparent)]"
+        >
+
+        <p>
+            I hope I was able to capture at least a bit of that vibe... It was a
+            really fun project to work on since I had the opportunity to learn
+            and experiment with some creative styling and animations that I've
+            never done before! You can check out the code for this website in my
+            GitHub or by clicking on
+            <a
+                href="https://github.com/matheus-pissaia/personal-page"
+                title="GitHub repository for this website"
+                target="_blank"
+                rel="noopener"
+                class="underline"
+            >
+                this repo link here.
+            </a>
+        </p>
+
+        <p>
+            Hope you enjoyed it and maybe even got some inspiration for your own projects!
+            <span class="text-18">👋</span>
+        </p>
+    </div>
 </template>
